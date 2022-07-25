d50cb9223103d29608e625fef37312c19cf1d0ca
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-25 16:48:32 -0400

    DID A GET AND POST IN EXPRESS

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 //1 install express
 
 //2 import the express library
-import  express  from 'express'
+import  express, { request, response }  from 'express'
 
 //3 initialize express server
 const app = express()
+app.use(express.json())
 
+//add a new student (in theory)
 
+app.post('/students', (request, response) => {
+    const newStudent = request.body
+    students.push(newStudent)
+    response.status(201).send(students)
+})
 
 //4 list request that we want express to respond to 
 app.get('/hello', (request, response) => {
@@ -15,6 +22,27 @@ app.get('/hello', (request, response) => {
     response.send('Hello World 🌎!')
 })
 
+const students = [
+    {firstName: 'Jonathan', lastName: 'Vegas'},
+    {firstName: 'Bridgette', lastName: 'Lemus'},
+    {firstName: 'Logan', lastName: 'McCalley'},
+    {firstName: 'Mason', lastName: 'Madara'},
+    {firstName: 'Sabastien', lastName: 'Placide'},
+]
+
+app.get('/students', (request, response) => {
+    response.send(students)
+})
+
+app.get('/students/:fName', (request, response) => {
+    const student = students.find(stud => stud.firstName === request.params.fName)
+    // this is if student does not exist 
+    if(!student){
+        response.status(404).send({message: 'Student not found', success: false})
+        return
+    }
+    response.send(student)
+})
 
 
 
